ba7d038de208b2bb55e22dcd7275bd06af5f29b7
Crane Chu <cranechu@gmail.com>
2019-09-21 11:28:56 +0800

    not build pdf for now

--- doc/conf.py
+++ doc/conf.py
@@ -43,10 +43,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# pdf needs to install more package for Chinese
-#https://www.xncoding.com/2017/01/22/fullstack/readthedoc.html
-formats: []
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
