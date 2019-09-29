bd2bd2e31767d4b2845197f5be043e86f5ad2f31
Crane Chu <cranechu@gmail.com>
2019-09-29 11:43:30 +0800

    compile in centos 8

--- install.sh
+++ install.sh
@@ -34,14 +34,16 @@
 
 
 if [ -s /etc/redhat-release ]; then
-  # fedora
-  sudo dnf install -y make redhat-rpm-config python3-devel python3-pip 
+    # fedora
+    sudo dnf install -y make redhat-rpm-config python3-devel python3-pip
+    # centos 8
+    sudo dnf install numactl-devel libaio-devel libuuid-devel openssl-devel
 elif [ -f /etc/debian_version ]; then
-  # ubuntu
-  sudo apt install -y python3-setuptools python3-dev python3-pip 
+    # ubuntu
+    sudo apt install -y python3-setuptools python3-dev python3-pip 
 else
-  echo "unknown system type."
-  exit 1
+    echo "unknown system type."
+    exit 1
 fi
 
 # get depended source code and software
@@ -56,7 +58,4 @@ cd spdk; git checkout pynvme_1.4; ./configure --without-isal; cd ..
 make spdk                                    # compile SPDK
 make                                         # compile pynvme
 
-echo ""
-echo "Congratulations, pynvme is installed successfully!"
-echo "To continue with VSCode, please refer to https://github.com/cranechu/pynvme#vscode"
-
+echo "pynvme install done."
