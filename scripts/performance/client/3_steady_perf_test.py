37239134508de2697f7a75b4b6f1b46fe33f9573
Crane Chu <cranechu@gmail.com>
2019-10-05 19:39:18 +0800

    script: add iops_consistency result

--- scripts/performance/client/3_steady_perf_test.py
+++ scripts/performance/client/3_steady_perf_test.py
@@ -15,11 +15,11 @@ def do_fill_drive(rand, nvme0n1, region_end):
     io_count = ns_size//io_size
     io_per_second = []
     
-    r = nvme0n1.ioworker(io_size=io_size, lba_align=io_size,
-                         lba_random=rand, qdepth=512,
-                         region_end=region_end,
-                         io_count=io_count, read_percentage=0,
-                         output_io_per_second=io_per_second).start().close()
+    nvme0n1.ioworker(io_size=io_size, lba_align=io_size,
+                     lba_random=rand, qdepth=512,
+                     region_end=region_end,
+                     io_count=io_count, read_percentage=0,
+                     output_io_per_second=io_per_second).start().close()
     logging.info(io_per_second)
 
     
@@ -38,15 +38,16 @@ def test_random_write_small_region(nvme0n1):
 # latency: read/write/mixed
 @pytest.mark.parametrize("readp", [100, 0, 67])
 def test_steady_iops_latency(nvme0n1, readp):
-    percentile_latency = dict.fromkeys([90, 99, 99.9, 99.999])
+    percentile_latency = dict.fromkeys([50, 90, 99, 99.9, 99.999])
     io_per_second = []
 
-    r = nvme0n1.ioworker(io_size=8, lba_align=8,
+    w = nvme0n1.ioworker(io_size=8, lba_align=8,
                          lba_random=rand, qdepth=64,
                          region_end=nvme0n1.id_data(7, 0)//5, 
                          time=1000, read_percentage=readp,
                          output_percentile_latency=percentile_latency, 
-                         output_io_per_second=io_per_second).start().close()    
-
+                         output_io_per_second=io_per_second).start()
+    w.close()
     logging.info(io_per_second)
+    logging.info(w.iops_consistency(90))
     logging.info(percentile_latency)
