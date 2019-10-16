aa746b660b8d6c904e13b1bbcd83f07dbccbf92b
Crane Chu <cranechu@gmail.com>
2019-10-16 13:10:43 +0800

    script: get DELL smart attributes

--- scripts/utility_test.py
+++ scripts/utility_test.py
@@ -37,7 +37,7 @@ def sg_show_hex_buffer(buf):
                              disabled=True, 
                              size=(80, 25))]
              ]
-    sg.Window(buf, layout, font=('monospace', 12)).Read()
+    sg.Window(str(buf), layout, font=('monospace', 12)).Read()
 
     
 def test_controller_identify_data(nvme0):
@@ -77,4 +77,25 @@ def test_sanitize(nvme0, nvme0n1, buf):
                                 'progress', orientation='h')
         nvme0.getlogpage(0x81, buf, 20).waitdone()
         time.sleep(1)
+
         
+def test_get_dell_smart_attributes(nvme0):
+    smart = d.Buffer()
+    nvme0.getlogpage(0xCA, smart, 512).waitdone()
+
+    l = []
+    l.append('Byte | Value | Attribute')
+    l.append('   0 |  %3d  | Re-Assigned Sector Count' % smart.data(0))
+    l.append('   1 |  %3d  | Program Fail Count (Worst Case Component)' % smart.data(1))
+    l.append('   2 |  %3d  | Program Fail Count (SSD Total)' % smart.data(2))
+    l.append('   3 |  %3d  | Erase Fail Count (Worst Case Component)' % smart.data(3))
+    l.append('   4 |  %3d  | Erase Fail Count (SSD Total)' % smart.data(4))
+    l.append('   5 |  %3d  | Wear Leveling Count' % smart.data(5))
+    l.append('   6 |  %3d  | Used Reserved Block Count (Worst Case Component)' % smart.data(6))
+    l.append('   7 |  %3d  | Used Reserved Block Count (SSD Total)' % smart.data(7))
+    l.append('11:8 |  %3d  | Reserved Block Count (SSD Total)' % smart.data(11, 8))
+
+    layout = [[sg.Listbox(l, size=(70, 10))]]
+    sg.Window("Dell SMART Attributes",
+              layout+[[sg.OK()]],
+              font=('monospace', 16)).Read()
