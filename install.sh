3a7985dcf9beee7960770324d2c61a1c183946e4
ssd <ssd@localhost.localdomain>
2019-11-04 13:14:54 +0800

    for centos 7

--- install.sh
+++ install.sh
@@ -34,8 +34,8 @@
 
 
 if [ -s /etc/redhat-release ]; then
-    # fedora
-    sudo dnf install -y make redhat-rpm-config python3-devel python3-pip python3-tkinter
+    # fedora, centos7
+    sudo yum install -y make redhat-rpm-config python3-devel python3-pip python3-tkinter
 elif [ -f /etc/debian_version ]; then
     # ubuntu
     sudo apt install -y python3-setuptools python3-dev python3-pip 
