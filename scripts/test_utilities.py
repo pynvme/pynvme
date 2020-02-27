116e45e851747b80a4762fa863717c8604b83ad4
Crane Chu <cranechu@gmail.com>
2020-02-27 15:14:12 +0800

    scripts: get smart data with human readable output

--- scripts/test_utilities.py
+++ scripts/test_utilities.py
@@ -84,18 +84,61 @@ def test_get_dell_smart_attributes(nvme0):
     nvme0.getlogpage(0xCA, smart, 512).waitdone()
 
     l = []
-    l.append('Byte | Value | Attribute')
-    l.append('   0 |  %3d  | Re-Assigned Sector Count' % smart.data(0))
-    l.append('   1 |  %3d  | Program Fail Count (Worst Case Component)' % smart.data(1))
-    l.append('   2 |  %3d  | Program Fail Count (SSD Total)' % smart.data(2))
-    l.append('   3 |  %3d  | Erase Fail Count (Worst Case Component)' % smart.data(3))
-    l.append('   4 |  %3d  | Erase Fail Count (SSD Total)' % smart.data(4))
-    l.append('   5 |  %3d  | Wear Leveling Count' % smart.data(5))
-    l.append('   6 |  %3d  | Used Reserved Block Count (Worst Case Component)' % smart.data(6))
-    l.append('   7 |  %3d  | Used Reserved Block Count (SSD Total)' % smart.data(7))
-    l.append('11:8 |  %3d  | Reserved Block Count (SSD Total)' % smart.data(11, 8))
+    l.append('Byte |  Value  | Attribute')
+    l.append('   0 |  %5d  | Re-Assigned Sector Count' % smart.data(0))
+    l.append('   1 |  %5d  | Program Fail Count (Worst Case Component)' % smart.data(1))
+    l.append('   2 |  %5d  | Program Fail Count (SSD Total)' % smart.data(2))
+    l.append('   3 |  %5d  | Erase Fail Count (Worst Case Component)' % smart.data(3))
+    l.append('   4 |  %5d  | Erase Fail Count (SSD Total)' % smart.data(4))
+    l.append('   5 |  %5d  | Wear Leveling Count' % smart.data(5))
+    l.append('   6 |  %5d  | Used Reserved Block Count (Worst Case Component)' % smart.data(6))
+    l.append('   7 |  %5d  | Used Reserved Block Count (SSD Total)' % smart.data(7))
+    l.append('11:8 |  %5d  | Reserved Block Count (SSD Total)' % smart.data(11, 8))
 
     layout = [[sg.Listbox(l, size=(70, 10))]]
+    sg.Window("Dell SMART Attributes",
+              layout+[[sg.OK()]],
+              font=('monospace', 16)).Read()
+
+
+def test_get_smart_health_information(nvme0):
+    smart = d.Buffer()
+    nvme0.getlogpage(0x02, smart, 512).waitdone()
+
+    l = []
+    l.append('  Byte |   Value   | Attribute')
+    l.append('     0 |  %7d  | Critical Warning' % smart.data(0))
+    l.append('  2: 1 |  %7d  | Composite Temperature (degree C)' % k2c(smart.data(2, 1)))
+    l.append('     3 |  %7d  | Available Spare' % smart.data(3))
+    l.append('     4 |  %7d  | Available Spare Threshold' % smart.data(4))
+    l.append('     5 |  %7d  | Percentage Used' % smart.data(5))
+    l.append('     6 |  %7d  | Endurance Group Critical Warning Summary' % smart.data(6))
+    l.append(' 47:32 |%11d| Data Units Read (1000LBA)' % smart.data(47, 32))
+    l.append(' 63:48 |%11d| Data Units Written (1000LBA)' % smart.data(63, 48))
+    l.append(' 79:64 |%11d| Host Read Commands' % smart.data(79, 64))
+    l.append(' 95:80 |%11d| Host Write Commands' % smart.data(95, 80))
+    l.append('111:96 |  %7d  | Controller Busy Time (minutes)' % smart.data(111, 96))
+    l.append('127:112|  %7d  | Power Cycles' % smart.data(127, 112))
+    l.append('143:128|  %7d  | Power On Hours' % smart.data(143, 128))
+    l.append('159:144|  %7d  | Unsafe Shutdowns' % smart.data(159, 144))
+    l.append('175:160|  %7d  | Media and Data Integrity Errors' % smart.data(175, 160))
+    l.append('191:176|  %7d  | Number of Error Information Log Entries' % smart.data(191, 176))
+    l.append('195:192|  %7d  | Warning Composite Temperature Time (minutes)' % smart.data(195, 192))
+    l.append('199:196|  %7d  | Critical Composite Temperature Time (minutes)' % smart.data(199, 196))
+    l.append('201:200|  %7d  | Temperature Sensor 1 (degree C)' % k2c(smart.data(201, 200)))
+    l.append('203:202|  %7d  | Temperature Sensor 2 (degree C)' % k2c(smart.data(203, 202)))
+    l.append('205:204|  %7d  | Temperature Sensor 3 (degree C)' % k2c(smart.data(205, 204)))
+    l.append('207:206|  %7d  | Temperature Sensor 4 (degree C)' % k2c(smart.data(207, 206)))
+    l.append('209:208|  %7d  | Temperature Sensor 5 (degree C)' % k2c(smart.data(209, 208)))
+    l.append('211:210|  %7d  | Temperature Sensor 6 (degree C)' % k2c(smart.data(211, 210)))
+    l.append('213:212|  %7d  | Temperature Sensor 7 (degree C)' % k2c(smart.data(213, 212)))
+    l.append('215:214|  %7d  | Temperature Sensor 8 (degree C)' % k2c(smart.data(215, 214)))
+    l.append('219:216|  %7d  | Thermal Management Temperature 1 Transition Count' % smart.data(219, 216))
+    l.append('223:220|  %7d  | Thermal Management Temperature 2 Transition Count' % smart.data(223, 220))
+    l.append('227:224|  %7d  | Total Time For Thermal Management Temperature 1' % smart.data(227, 224))
+    l.append('231:228|  %7d  | Total Time For Thermal Management Temperature 2' % smart.data(231, 228))
+
+    layout = [[sg.Listbox(l, size=(70, 20))]]
     sg.Window("Dell SMART Attributes",
               layout+[[sg.OK()]],
               font=('monospace', 16)).Read()
