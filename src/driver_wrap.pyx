1a5e0c7102a7eda8edc05f9cc2d53de4685484a0
cranechu <cranechu@gmail.com>
2020-07-16 14:21:54 +0800

    driver: rescan after binding uio driver

--- src/driver_wrap.pyx
+++ src/driver_wrap.pyx
@@ -430,8 +430,8 @@ cdef class Subsystem(object):
         pcie = self._nvme.pcie
 
         # config spdk driver
-        pcie._rescan()
         pcie._bind_driver('uio_pci_generic')
+        pcie._rescan()
         logging.info("reset controller to use it after power on")
         return True
 
@@ -511,8 +511,8 @@ cdef class Subsystem(object):
         self._nvme[0x20] = 0x4e564d65  # "NVMe"
 
         # config spdk driver
-        pcie._rescan()
         pcie._bind_driver('uio_pci_generic')
+        pcie._rescan()
         logging.info("reset controller to use it after subsystem reset")
         return True
 
@@ -712,8 +712,6 @@ cdef class Pcie(object):
             subprocess.call('echo "%s" > "/sys/bus/pci/drivers/%s/bind" 2> /dev/null || true' % (bdf, driver), shell=True)
             logging.debug("bind %s on %s" % (driver, bdf))
 
-        self._rescan()
-
     def reset(self):  # pcie
         """reset this pcie device with hot reset
 
@@ -742,8 +740,8 @@ cdef class Pcie(object):
         time.sleep(0.5)
 
         # config spdk driver
-        self._rescan()
         self._bind_driver('uio_pci_generic')
+        self._rescan()
         logging.info("reset controller to use it after pcie reset")
         return True
 
