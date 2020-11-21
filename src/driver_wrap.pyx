128556c224edcf8299c796b68eb257751a5ff7b5
Crane Chu <cranechu@gmail.com>
2020-11-21 23:17:59 +0800

    add ps scripts

--- src/driver_wrap.pyx
+++ src/driver_wrap.pyx
@@ -1170,10 +1170,10 @@ cdef class Controller(object):
         for i in range(_aer_resend):
             # send more aer commands
             self.aer(cb=self.aer_cb_func)
-        assert _aer_waitdone >= reaped-expected, "deduct pended aer commands"
-        for i in range(_aer_waitdone-(reaped-expected)):
-            # process one more command in lieu of aer completion
-            self.waitdone()
+        if _aer_waitdone >= reaped-expected:
+            for i in range(_aer_waitdone-(reaped-expected)):
+                # process one more command in lieu of aer completion
+                self.waitdone()
 
         return _latest_cqe_cdw0
 
